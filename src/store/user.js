cbd582277d2ada72a012c34e4213c9119fda1a08
saravy meas <saravy.meas.007@gmail.com>
2024-12-20 21:52:43 +0700

    add cors in backend

--- src/store/user.js
+++ src/store/user.js
@@ -15,12 +15,15 @@ export const useUsersStore = defineStore("user", {
     },
     async updateProfileImage(userId, token) {
       try {
-        const response = await axios.get("/api/user-profile/${userId}", {
-          responseType: "blob",
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const response = await axios.get(
+          `${apiBaseUrl}/user-profile/${userId}`,
+          {
+            responseType: "blob",
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
         this.profileImage = URL.createObjectURL(response.data);
       } catch (error) {
         if (error.response) {
@@ -40,7 +43,7 @@ export const useUsersStore = defineStore("user", {
     },
     async login(userEmail, userPassword) {
       try {
-        const { data } = await axios.post("/api/auth/login", {
+        const { data } = await axios.post(`${apiBaseUrl}/auth/login`, {
           email: userEmail,
           password: userPassword,
         });
@@ -82,7 +85,7 @@ export const useUsersStore = defineStore("user", {
       confirmPassword
     ) {
       try {
-        const { data } = await axios.post("/api/auth/signup", {
+        const { data } = await axios.post(`${apiBaseUrl}/auth/signup`, {
           firstName: firstName,
           lastName: lastName,
           gender: gender,
